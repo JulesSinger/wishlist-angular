db7bd9dcc24d8d2e32a5b5e5f93bc9b33d11138b
SINGER Jules <jules.singer@telecomnancy.eu>
2023-11-01 01:27:22 +0100

    feat: initialize project with data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { WishItem } from 'src/shared/models/wishItem';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  items = [
+    new WishItem('An iPhone', true),
+    new WishItem('A MacBook'),
+    new WishItem('A Tesla')
+  ];
   title = 'wishlist';
 }
