00ffcfabf3a7592a37510839b0d53d88f6cf431a
SINGER Jules <jules.singer@telecomnancy.eu>
2023-11-02 23:56:07 +0100

    feat: work with ngModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,38 @@ import { WishItem } from 'src/shared/models/wishItem';
 })
 export class AppComponent {
   items : WishItem[] = [
-    new WishItem('An iPhone', true),
-    new WishItem('A MacBook'),
-    new WishItem('A Tesla')
+    new WishItem('New Laptop'),
+    new WishItem('Trip to Hawaii'),
+    new WishItem('New Car')
+
+
   ];
+  newWishText : string = '';
+  listFilter: string = '0';
   title = 'wishlist';
+  visibleItems: WishItem[] = this.items;
+  
 
   toggleItem(item: WishItem) {
     item.isComplete = !item.isComplete;
+  }
+
+  addNewWish() {
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+  }
+
+  filterChanged(value: string) {
+    this.listFilter = value;
+
+    if(value === '0') {
+      this.visibleItems = this.items;
+    }
+    else if(value === '2') {
+      this.visibleItems = this.items.filter(item => !item.isComplete);
+    }
+    else {
+      this.visibleItems = this.items.filter(item => item.isComplete);
+    }
   }
 }
